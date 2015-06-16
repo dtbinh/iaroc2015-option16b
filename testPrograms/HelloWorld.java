43cbfe147646adb30a4afb279c5e66860dc06f14
rottino <rachelottino6@gmail.com>
2015-06-16 12:10:20 -0700

    Upload test program

---
+++ testPrograms/HelloWorld.java
@@ -0,0 +1,12 @@
+package testPrograms;
+
+import lejos.nxt.Button;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+		Button.waitForAnyPress();
+	}
+}
